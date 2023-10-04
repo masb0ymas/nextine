b439b1f305212fb80c539cdf70682dcf45619c28
masb0ymas <n.fajri@outlook.com>
2023-10-04 10:37:21 +0700

    fix: component table scroll

--- src/core/components/MyTable/TableScroll/TableScroll.tsx
+++ src/core/components/MyTable/TableScroll/TableScroll.tsx
@@ -1,8 +1,9 @@
 import { ScrollArea, Table } from '@mantine/core'
 import cx from 'clsx'
 import { useState } from 'react'
-import classes from './TableScroll.module.css'
+import MyPaper from '~/core/components/MyPaper/MyPaper'
 import { mockDataTable } from './mockTable'
+import classes from './table_scroll.module.css'
 
 export default function TableScroll() {
   const [scrolled, setScrolled] = useState(false)
@@ -16,22 +17,24 @@ export default function TableScroll() {
   ))
 
   return (
-    <ScrollArea
-      h={300}
-      onScrollPositionChange={({ y }) => setScrolled(y !== 0)}
-    >
-      <Table miw={700}>
-        <Table.Thead
-          className={cx(classes.header, { [classes.scrolled]: scrolled })}
-        >
-          <Table.Tr>
-            <Table.Th>Name</Table.Th>
-            <Table.Th>Email</Table.Th>
-            <Table.Th>Company</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>{rows}</Table.Tbody>
-      </Table>
-    </ScrollArea>
+    <MyPaper withBorder p={20} radius="md">
+      <ScrollArea
+        h={300}
+        onScrollPositionChange={({ y }) => setScrolled(y !== 0)}
+      >
+        <Table miw={700}>
+          <Table.Thead
+            className={cx(classes.header, { [classes.scrolled]: scrolled })}
+          >
+            <Table.Tr>
+              <Table.Th>Name</Table.Th>
+              <Table.Th>Email</Table.Th>
+              <Table.Th>Company</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>{rows}</Table.Tbody>
+        </Table>
+      </ScrollArea>
+    </MyPaper>
   )
 }
