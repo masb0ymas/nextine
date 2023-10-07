41065ea8453e4052e5069796a869ce71295607ab
masb0ymas <n.fajri@outlook.com>
2023-10-07 09:58:58 +0700

    fix: improve modal item detail

--- src/core/components/MyModal/partials/ModalItemDetail.tsx
+++ src/core/components/MyModal/partials/ModalItemDetail.tsx
@@ -1,8 +1,9 @@
 import { Checkbox, Divider, Group, Text } from '@mantine/core'
+import _ from 'lodash'
 import Image from 'rc-image'
-import classes from '../modal.module.css'
-import { formatDateText, formatDateTime } from '~/core/utils/date'
 import { formatCurrencyIDR } from '~/core/utils/currency'
+import { formatDateText, formatDateTime } from '~/core/utils/date'
+import classes from '../modal.module.css'
 
 interface ItemProps<T> {
   item: T
@@ -29,6 +30,8 @@ export enum ItemType {
 export default function ModalItemDetail<T>({ item, content }: ItemProps<T>) {
   const { title, key, type } = content
 
+  const data = _.get(item, key, '-')
+
   return (
     <>
       <Group justify="space-between">
@@ -38,37 +41,37 @@ export default function ModalItemDetail<T>({ item, content }: ItemProps<T>) {
 
         {type === ItemType.date && (
           // @ts-ignore
-          <Text size="sm">{formatDateText(item[key])}</Text>
+          <Text size="sm">{formatDateText(data)}</Text>
         )}
 
         {type === ItemType.date_time && (
           // @ts-ignore
-          <Text size="sm">{formatDateTime(item[key])}</Text>
+          <Text size="sm">{formatDateTime(data)}</Text>
         )}
 
         {type === ItemType.currency && (
           // @ts-ignore
-          <Text size="sm">{formatCurrencyIDR(item[key])}</Text>
+          <Text size="sm">{formatCurrencyIDR(data)}</Text>
         )}
 
         {type === ItemType.string && (
           // @ts-ignore
-          <Text size="sm">{item[`${key}`]}</Text>
+          <Text size="sm">{data}</Text>
         )}
 
         {type === ItemType.boolean && (
           // @ts-ignore
-          <Checkbox checked={Boolean(item[key])} />
+          <Checkbox checked={Boolean(data)} />
         )}
 
         {type === ItemType.image && (
           // @ts-ignore
-          <Image alt={'image'} src={item[key]?.signed_url} />
+          <Image alt={'image'} src={data} />
         )}
 
         {type === ItemType.html && (
           // @ts-ignore
-          <div dangerouslySetInnerHTML={{ __html: item[key] ?? '' }} />
+          <div dangerouslySetInnerHTML={{ __html: data }} />
         )}
       </Group>
 
